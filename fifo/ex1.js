bdfeb0fbc48f064db8f73b03dffd1776b6f48ad6
satorutoji <ezdummyacc@gmail.com>
2025-03-25 04:47:54 +0500

    fifo

--- fifo/ex1.js
+++ fifo/ex1.js
@@ -1,14 +1,14 @@
 class fifo_cache {
     constructor() {
-        this.cache = new Map()
+        this.cache = new Map() // Удобнее массива / объкта
     }
 
     set(key, value) {
-        this.cache.set(key, value)
+        this.cache.set(key, value) // Без ограничений добавляем пары key : value
     }
 
     get(key) {
-        return this.cache.get(key)
+        return this.cache.has(key) ? this.cache.get(key) : undefined // Проверяем прежде чем вернуть
     }
 }
 
@@ -17,6 +17,4 @@ test.set('a', 1)
 test.set('b', 2)
 test.set('c', 3)
 test.set('d', 4)
-
 console.log(test.get('c'))
-console.log(test)
---
+++ fifo/ex2.js
@@ -0,0 +1,31 @@
+class fifo_cache {
+    constructor(size) {
+        if (this.size <= 0)
+            // Проверка чтоб ты не создал алгоритм на 0 ячеек
+            throw new Error('Не создавай алгоритм с 0 ячеек балбес')
+        this.size = size
+        this.cache = new Map()
+    }
+    set(key, value) {
+        this.proverka()
+        this.cache.set(key, value)
+    }
+    get(key) {
+        return this.cache.has(key) ? this.cache.get(key) : undefined
+    }
+    proverka() {
+        if (this.cache.size >= this.size) {
+            // proverka удалит лишнюю (первую/старую) пару, если колекция переполнится
+            const firstKey = this.cache.keys().next().value //идея fifo "первым зашел - первым удалился"
+            this.cache.delete(firstKey)
+        }
+    }
+}
+
+const test = new fifo_cache(3)
+test.set('a', 1)
+test.set('b', 2)
+test.set('c', 3)
+test.set('d', 4)
+console.log(test.get('a'))
+console.log(test.get('c'))
